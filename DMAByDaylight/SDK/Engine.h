9670db54347f3f008c00f1a5936fcefd4823933a
SoTMaulder <158225586+SoTMaulder@users.noreply.github.com>
2025-02-04 17:52:34 -0500

    updated gworld offset
    updated gworld offset

--- DMAByDaylight/SDK/Engine.h
+++ DMAByDaylight/SDK/Engine.h
@@ -13,13 +13,13 @@ private:
 	uint64_t MaxPacket;
 	uint64_t OwningGameInstance = 0x1c8; // World -> OwningGameInstance
 	uint64_t PersistentLevel = 0x38; // World  -> PersistentLevel
-	uint64_t GWorld = 0xA8AE260;
+	uint64_t GWorld = 0xA8BD9F0;
 	uint64_t LocalPlayers = 0x40; // GameInstance -> LocalPlayers
 	uint64_t PlayerController = 0x38; // Player -> PlayerController
 	uint64_t AcknowledgedPawn = 0x350;	
 	uint64_t CameraManager = 0x360; // PlayerController -> PlayerCameraManager
 	uint64_t CameraCachePrivate = 0x0; // PlayerCameraManager -> CameraCachePrivate
-	uint64_t CameraCachePrivateOffset = 0x22d0; // PlayerCameraManager -> CameraCachePrivate
+	uint64_t CameraCachePrivateOffset = 0x22D0; // PlayerCameraManager -> CameraCachePrivate
 	CameraCacheEntry CameraEntry; // ScriptStruct Engine.CameraCacheEntry
 	MinimalViewInfo CameraViewInfo; // ScriptStruct Engine.MinimalViewInfo
 	std::vector<std::shared_ptr<ActorEntity>> Actors;
