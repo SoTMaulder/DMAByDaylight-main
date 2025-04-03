0e41d544654be45635cd3d904b39c1458ea096f0
SoTMaulder <158225586+SoTMaulder@users.noreply.github.com>
2025-04-02 21:43:08 -0400

    Updated to latest patch
    Updated to latest patch

--- DMAByDaylight/SDK/Engine.h
+++ DMAByDaylight/SDK/Engine.h
@@ -13,10 +13,10 @@ private:
 	uint64_t MaxPacket;
 	uint64_t OwningGameInstance = 0x1c8; // World -> OwningGameInstance
 	uint64_t PersistentLevel = 0x38; // World  -> PersistentLevel
-	uint64_t GWorld = 0xA8C7160;
+	uint64_t GWorld = 0xA9A0270;
 	uint64_t LocalPlayers = 0x40; // GameInstance -> LocalPlayers
 	uint64_t PlayerController = 0x38; // Player -> PlayerController
-	uint64_t AcknowledgedPawn = 0x350;	
+	uint64_t AcknowledgedPawn = 0x350;	// Playercontroller -> AcknowledgedPawn
 	uint64_t CameraManager = 0x360; // PlayerController -> PlayerCameraManager
 	uint64_t CameraCachePrivate = 0x0; // PlayerCameraManager -> CameraCachePrivate
 	uint64_t CameraCachePrivateOffset = 0x22D0; // PlayerCameraManager -> CameraCachePrivate
